4750a72fc8784263256c966eff22b52baf63e786
nathalidev <nathalidorio@gmail.com>
2025-08-20 22:48:44 -0300

    list_comprehension.2

--- exercicios_curso_python/introducao_list_comprehension.py
+++ exercicios_curso_python/introducao_list_comprehension.py
@@ -4,5 +4,10 @@ print(lista_cubos)
 lista_maiusculas = [x.capitalize() for x in ["oi", "alô", "tchau"]]
 print(lista_maiusculas)
 
-lista_matrix = [x.remove()for x, y in [[1,2],[3,4],[5]] if x[y] > 2]
-print(lista_matrix)
+matriz = [[1, 2], [3, 4], [5]]
+
+# recriando a matriz, mas só com elementos maiores que 2
+nova_matriz = [[x for x in linha if x > 2] for linha in matriz]
+
+print(nova_matriz)
+# Saída: [[], [3, 4], [5]]
