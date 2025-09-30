41bee3cadbc4557fa7f66c199cb3dcdf086f53e6
nathalidev <nathalidorio@gmail.com>
2025-09-30 16:54:42 -0300

    componentizado, o arquivo json que serve como banco de dados agora tem uma função para preparar o arquivo para leitura e para gravação de conteudo

--- exercicios_curso_python/CRUDzinho.py
+++ exercicios_curso_python/CRUDzinho.py
@@ -2,16 +2,25 @@ import json
 import os
 import sys
 
+def carregar_json(nome_arquivo):
+    if os.path.exists(nome_arquivo): # Verifica se o arquivo "tarefas.json" existe no diretório atual
+        with open(nome_arquivo, "r", encoding="utf-8") as f: # Abre o arquivo "tarefas.json" no modo leitura ('r')
+            arquivo_leitura = json.load(f)  # Lê o conteúdo JSON do arquivo e transforma em objeto Python (lista, dicionário, etc.)
+
+    else:
+        arquivo_leitura=[]
+
+    return arquivo_leitura
+
+def editar_json(nome_arquivo, arquivo_leitura):
+    with open(nome_arquivo, "w", encoding="utf-8") as f: # Abre o arquivos "tarefas.json" no modo escrita ('w')
+        json.dump(arquivo_leitura, f, indent=4, ensure_ascii=False) # lista_tarefas é a estrutura de dados que quero salvar no arquivo 
+        # "f" é o arquivo aberto pronto para ter mais conteúdo gravado em si 
+
 while True:
     escolha_usuario = int(input("\n1 - Adicionar tarefa\n2 - Listar tarefas\n3 - Marcar como concluída ou pendente\n4 - Remover tarefa\n5 - Sair\nSua opção: "))
 
-    if os.path.exists("tarefas.json"): # Verifica se o arquivo "tarefas.json" existe no diretório atual
-        with open("tarefas.json", "r", encoding="utf-8") as f: # Abre o arquivo "tarefas.json" no modo leitura ('r')
-            lista_tarefas = json.load(f)  # Lê o conteúdo JSON do arquivo e transforma em objeto Python (lista, dicionário, etc.)
-
-    else:
-        lista_tarefas=[]
-
+    lista_tarefas = carregar_json("tarefas.json")
     try: 
         if escolha_usuario == 1:
             descricao = input("Digite a descrição da tarefa: ")
@@ -30,9 +39,7 @@ while True:
                 f"{descricao} [{concluida}]"
             )
 
-            with open("tarefas.json", "w", encoding="utf-8") as f: # Abre o arquivos "tarefas.json" no modo escrita ('w')
-                json.dump(lista_tarefas, f, indent=4, ensure_ascii=False) # lista_tarefas é a estrutura de dados que quero salvar no arquivo 
-                # "f" é o arquivo aberto pronto para ter mais conteúdo gravado em si 
+            editar_json("tarefas.json", lista_tarefas)
 
         if escolha_usuario == 2:
             for id, tarefa in enumerate(lista_tarefas, start=1):
@@ -49,12 +56,10 @@ while True:
             else:
                 lista_tarefas[selecao_tarefa-1]=lista_tarefas[selecao_tarefa-1].replace("Pendente", "Concluída") 
 
-            with open("tarefas.json", "w", encoding="utf-8") as f:
-                json.dump(lista_tarefas, f, indent=4, ensure_ascii=False)
+            editar_json("tarefas.json", lista_tarefas)
 
             for tarefa in lista_tarefas:
                 print(f"\n{tarefa}")
-
 
         if escolha_usuario == 4:
             for tarefa in lista_tarefas:
@@ -62,14 +67,10 @@ while True:
             selecao_tarefa = int(input("\nInforme o id da tarefa a ser excluida: "))
             lista_tarefas.pop(selecao_tarefa-1)
 
-            with open("tarefas.json", "w", encoding="utf-8") as f:
-                json.dump(lista_tarefas, f, indent=4, ensure_ascii=False)
+            editar_json("tarefas.json", lista_tarefas)
 
         if escolha_usuario == 5:
             sys.exit()
-
-        # else:
-        #     print("Opção inválida")
 
     except Exception as e:
         print(f"ERRO: {e}")
