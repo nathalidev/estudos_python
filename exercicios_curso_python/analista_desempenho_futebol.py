b29f0baa2868285956a5813acd3f2d4f9862bd74
nathalidev <nathalidorio@gmail.com>
2026-05-11 14:54:41 -0300

    atualiza avaliador de jogador para incluir posição e calcular índice de desempenho corretamente

--- exercicios_curso_python/analista_desempenho_futebol.py
+++ exercicios_curso_python/analista_desempenho_futebol.py
@@ -3,25 +3,39 @@ def aplicar_metricas(funcao, *args):
 
 def avaliador_de_jogador(nome_do_jogador):
     def funcao_interna(*args):
-        gols, assistencias, passes = args
+        gols, assistencias, passes, posicao = args
         jogador = {}
 
         jogador['Nome'] = nome_do_jogador
 
+        jogador['Posição'] = posicao
 
         jogador['Total de Gols'] = gols
-
 
         jogador['Total de Assistências'] = assistencias
 
         jogador['Passes certos'] = passes
 
-        jogador['Indice de desempenho'] = (jogador['Total de Gols'] * 4 + jogador['Total de Assistências'] * 3 + jogador['Passes certos'] * 0.1)
+        if posicao == 'Atacante':
+            jogador['Indice de desempenho'] = (jogador['Total de Gols'] * 5 + jogador['Total de Assistências'] * 3 + jogador['Passes certos'] * 0.05)
 
-        return jogador
+        elif posicao == 'Meio-campista':
+            jogador['Indice de desempenho'] = (jogador['Total de Gols'] * 3 + jogador['Total de Assistências'] * 4 + jogador['Passes certos'] * 0.2)
+
+        else: 
+            jogador['Indice de desempenho'] = (jogador['Total de Gols'] * 2 + jogador['Total de Assistências'] * 2 + jogador['Passes certos'] * 0.3)
+
+        avaliacao_indice = 'Excelente' if jogador['Indice de desempenho'] >= 101 else 'Muito bom' if jogador['Indice de desempenho'] >= 71 else 'Bom' if jogador['Indice de desempenho'] >= 41 else 'Regular' if jogador['Indice de desempenho'] >= 21 else 'Fraco'
+
+        return f'{jogador}/nAvaliação Geral: {avaliacao_indice}'
     return funcao_interna
 
-avaliar = avaliador_de_jogador("Nathã")
-resultado = aplicar_metricas(avaliar, 3, 18, 450)
+nome_do_jogador = input("Digite o nome do jogador a ser avaliado: ")
+posicao = input("Digite a posição do jogador (Atacante, Meio-campista, Defensor): ")
+gols = int(input("Digite o número de gols marcados: "))
+assistencias = int(input("Digite o número de assistências feitas: "))
+passes = int(input("Digite o número de passes certos: "))
+avaliar = avaliador_de_jogador(nome_do_jogador)
+resultado = aplicar_metricas(avaliar, gols, assistencias, passes, posicao)
 print(resultado)
 
