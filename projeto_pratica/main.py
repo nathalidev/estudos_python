a5ead6a665bd959fc14a2e986da85c3657be3ba2
nathalidev <nathalidorio@gmail.com>
2026-05-28 13:09:48 -0300

    adiciona página de criação de orçamento e ajusta navegação no index

--- projeto_pratica/main.py
+++ projeto_pratica/main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/criar-orcamento")
+def criar_orcamento():
+    return render_template("criar_orcamento.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
