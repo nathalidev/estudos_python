661ba8a11fd2668b105aa7fbc265b93dfd630a18
nathalidev <nathalidorio@gmail.com>
2026-05-29 16:18:57 -0300

    adiciona função para exibir valores no formulário e corrige seleção de classe

---
+++ projeto_pratica/static/js/preview.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const regex = /^[A-Za-zÀ-ÿ\s]+$/;
+    function pegarValorExibir() {
+        const preview = document.querySelector('.preview-vazia');
+        const nomeCliente = document.getElementById("nome").value.trim();
+        const servicoItem = document.getElementById("servico_item").value.trim();
+        const valorUnitario = document.getElementById("valor").value
+    }
+
+});
