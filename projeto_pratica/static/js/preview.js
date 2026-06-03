8ff79adc9b82534d22bf6b75ae43e213b3649f39
nathalidev <nathalidorio@gmail.com>
2026-06-03 15:28:02 -0300

    ajuste para função de capturar de inputs na tela de criação de orçamento e renderização do valor desse input combinando html + js

--- projeto_pratica/static/js/preview.js
+++ projeto_pratica/static/js/preview.js
@@ -4,7 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const preview = document.querySelector('.preview-vazia');
         const nomeCliente = document.getElementById("nome").value.trim();
         const servicoItem = document.getElementById("servico_item").value.trim();
-        const valorUnitario = document.getElementById("valor").value
+        const valorUnitario = document.getElementById("valor").value;
+        const descricao = document.getElementById("descricao").value.trim();
+        const observacoes = document.getElementById("observacoes").value.trim();
+        const prazoEntrega = document.getElementById("prazo").value;
+
+        const teste = document.getElementById("meu-preview");
+        teste.textContent = `${nomeCliente}`
+
     }
 
+    document.getElementById("nome").addEventListener("input", pegarValorExibir);
 });
