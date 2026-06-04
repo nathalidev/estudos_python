b76221cb16b2b79f433108e00e1234c468fc042e
nathalidev <nathalidorio@gmail.com>
2026-06-04 20:06:58 -0300

    inserção de campos no formulario de criação do orçamento e ajuste na navbar da pagina principal

--- projeto_pratica/static/js/preview.js
+++ projeto_pratica/static/js/preview.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const hoje = new Date().toISOString().split("T")[0];
+    const validadeInput = document.getElementById("validade");
+    validadeInput.min = hoje;
+    validadeInput.max = "9999-12-31";
+
     const regex = /^[A-Za-zÀ-ÿ\s]+$/;
+
+    function exibirValidade(){
+        const campoValidade = document.getElementById('campo-validade');
+        const opcoesValidade = document.querySelectorAll('input[name="validade"]');
+
+        opcoesValidade.forEach(radio => {
+            radio.addEventListener('change', function() {
+            if (this.value === "sim") {
+                campoValidade.style.display = "block"; // mostra o campo
+            } else {
+                campoValidade.style.display = "none";  // esconde o campo
+            }
+            });
+        });
+    }
     function pegarValorExibir() {
         const preview = document.querySelector('.preview-vazia');
         const nomeCliente = document.getElementById("nome").value.trim();
