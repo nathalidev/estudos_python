2b657576659dd4d076296d29f44bf734e9264a11
nathalidev <nathalidorio@gmail.com>
2026-06-28 23:02:16 -0300

    inclusão de campos enviados da tela de criação do orçamento para o orçamento em si (front -> back)
    inserção de calculos vindos do backend ao orçamento a ser criado

--- projeto_pratica/app/routes/paginas_criacao_orcamento.py
+++ projeto_pratica/app/routes/paginas_criacao_orcamento.py
@@ -3,15 +3,17 @@ from flask import jsonify # função para retorno de dados em json pro front usa
 from flask import redirect, url_for #joga o usuário pra url em questão
 from app import app
 
+from datetime import datetime
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/criar-orcamento", methods=["GET", "POST"])
+@app.route("/criar-orcamento")
 def criar_orcamento():
     return render_template("criar_orcamento.html")
 
-@app.route('/orcamento')
+@app.route('/orcamento', methods=["GET", "POST"])
 def orcamento():
     nome_sua_empresa = request.form.get("sua_empresa")
     email_sua_empresa = request.form.get("email")
@@ -23,22 +25,28 @@ def orcamento():
     email_cliente = request.form.get("email_cliente")
 
     inserir_validade = request.form.get("inserir_validade")
-    data_validade = request.form.get("data_validade")
+    data_validade_ingles = request.form.get("data_validade")
+    data_validade = datetime.strptime(data_validade_ingles, "%Y-%m-%d").strftime("%d/%m/%Y") if data_validade_ingles else None
 
     nome_servico_item = request.form.get("servico_item")
-    quantidade_item = request.form.get("quantidade")
-    valor_unitario = request.form.get("valor")
-    valor_desconto = request.form.get("desconto")
+    quantidade_item = int(request.form.get("quantidade"))
+    valor_unitario = float(request.form.get("valor"))
+    valor_desconto = float(request.form.get("desconto"))
 
     observacoes_adicionais = request.form.get("observacoes")
-    prazo_entrega = request.form.get("prazo")
+    prazo_entrega_ingles = request.form.get("prazo")
+    prazo_entrega = datetime.strptime(prazo_entrega_ingles, "%Y-%m-%d").strftime("%d/%m/%Y") if prazo_entrega_ingles else None
     condicoes_pagamento = request.form.get("condicoes_pagamento")
+    data_hoje = datetime.now().strftime("%d/%m/%Y")
+    subtotal = quantidade_item * valor_unitario
+    total = subtotal - valor_desconto
 
     context = {
         "nome_sua_empresa": nome_sua_empresa,
         "email_sua_empresa": email_sua_empresa,
         "site_sua_empresa": site_sua_empresa,
         "telefone_sua_empresa": telefone_sua_empresa,
+        "data_hoje": data_hoje,
 
         "nome_cliente": nome_cliente,
         "telefone_cliente": telefone_cliente,
@@ -54,7 +62,9 @@ def orcamento():
 
         "observacoes_adicionais": observacoes_adicionais,
         "prazo_entrega": prazo_entrega,
-        "condicoes_pagamento": condicoes_pagamento
+        "condicoes_pagamento": condicoes_pagamento,
+        "subtotal": subtotal,
+        "total": total
     }
 
     return render_template('components/orcamento.html', **context)
