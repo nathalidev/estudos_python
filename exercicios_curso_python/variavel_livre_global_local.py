d34582b1192124e02c514e8b3bdc064e379581fb
nathalidev <nathalidorio@gmail.com>
2026-07-09 21:40:58 -0300

    adição do print para que sejam visualizados:
    -Nome da variavel livre
    
    -Endereço de memória de cada cell que contem uma variavel livre da closure alem do tipo e do endereço de memória do objeto que esse endereço referencia
    
    -Variáveis do escopo atual
    
    -Variáveis do escopo global

--- exercicios_curso_python/variavel_livre_global_local.py
+++ exercicios_curso_python/variavel_livre_global_local.py
@@ -8,6 +8,10 @@ def criar_contador():
     return adicionar
 
 contador = criar_contador()
+print(contador.__code__.co_freevars) # nome da variavel livre
+print(contador.__closure__) # endereço de memória de cada cell que contem uma variavel livre da closure alem do tipo e do endereço de memória do objeto que esse endereço referencia
+print(locals()) # variáveis do escopo atual
+print(globals()) # variáveis do escopo global
 while True:
     mensagem = input("Digite uma mensagem (ou 'sair' para encerrar): ")
     if mensagem.lower() == "sair":
