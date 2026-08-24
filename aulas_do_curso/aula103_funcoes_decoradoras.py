00e0e9f51c101095cb7f8bf2041573c33e904e82
nathalidev <nathalidorio@gmail.com>
2026-08-24 20:35:34 -0300

    COMENTÁRIOS EXPLICATIVOS SOBRE FUNÇÕES DECORADORAS

--- aulas_do_curso/aula103_funcoes_decoradoras.py
+++ aulas_do_curso/aula103_funcoes_decoradoras.py
@@ -8,6 +8,7 @@ def criar_funcao(func):
     def interna(*args, **kwargs):
         print('Vou te decorar')
         for arg in args:
+            print(f'Checando se {arg} é uma string')
             e_string(arg)
         resultado = func(*args, **kwargs)
         print(f'O seu resultado foi {resultado}.')
@@ -28,3 +29,12 @@ def e_string(param):
 inverte_string_checando_parametro = criar_funcao(inverte_string)
 invertida = inverte_string_checando_parametro('123')
 print(invertida)
+
+# Fluxo de execução:
+# 1. A função `criar_funcao` é chamada com a função `inverte_string` como argumento.
+# 2. E o que criar_funcao com inverte_string no argumento retornaria? Ora de qualquer forma ela retorna a função interna, que é a função `interna`. Então, `inverte_string_checando_parametro` agora é uma referência para a função `interna`.
+# 3. Quando chamamos `inverte_string_checando_parametro('123')`, estamos chamando a função `interna` com o argumento `'123'`.
+# 4. Dentro da função `interna`, ele imprime "Vou te decorar", e depois entra no loop `for` para verificar se cada argumento é uma string usando a função `e_string`. 
+# 5. Como `'123'` é uma string, não ocorre nenhum erro.
+# 6. Em seguida, a função `func` (que é `inverte_string`) é chamada com o argumento `'123'`, e o resultado é armazenado na variável `resultado`. A função `inverte_string` retorna `'321'.
+# 7. Ou seja invertida passa a ser `'321'`.
