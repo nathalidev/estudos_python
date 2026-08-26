b640966eccda8cfb491e9c717b968167aacde2c8
nathalidev <nathalidorio@gmail.com>
2026-08-26 15:00:30 -0300

    exercicio de união de listas de tamanhos diferentes unindo as lsitas termo a termo até o final da lista menor onde a cada termo das duas listas é criado uma tupla que ao final entra em uma lista

---
+++ aulas_do_curso/aula107_unir_listas.py
@@ -0,0 +1,19 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+def zipper(cidades: list , siglas: list):
+    lista_final = []
+    for x, y in zip(cidades, siglas):
+        cidade_estado = (x, y)
+        lista_final.append(cidade_estado)
+    print(lista_final)
+
+zipper(['Salvador', 'Ubatuba', 'Belo Horizonte'], ['BA', 'SP', 'MG', 'RJ'])
