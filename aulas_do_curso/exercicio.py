c74986e419fd6f8ad8b25cc57e767c4160809f47
nathalidev <nathalidorio@gmail.com>
2026-08-28 16:29:38 -0300

    progesso até o momento no exercicio de lógica

--- aulas_do_curso/exercicio.py
+++ aulas_do_curso/exercicio.py
@@ -12,24 +12,38 @@
 # Entrada: [5, 2, 9, 1, 5, 6, 2]
 # Saída:   [1, 2, 5, 6, 9]
 
-def retirar_repetidos(lista):
+def tamanho_lista(lista):
+    contador = 0
+    for termo in lista:
+        contador+=1
+    return contador
+
+def retirar_repetidos(lista, tamanho_total):
     inicio_lista_a_comparar = 1
     indice_lista_repetidos = 0
     lista_repetidos = [0]
-    delimitador_lista_repetidos = 1
-    contador_passagem = 0
+    delimitador_lista_repetidos = 0
+    contador_comparacao = 0
     for termo in lista:
         if termo in lista_repetidos:
             continue
         else:
             for termo_comparacao in lista[inicio_lista_a_comparar::]:
-                inicio_lista_a_comparar +=1
+                contador_comparacao += 1
                 if termo == termo_comparacao:
-                    lista_repetidos = lista_repetidos * delimitador_lista_repetidos
                     delimitador_lista_repetidos +=1
+                    lista_repetidos = lista_repetidos * delimitador_lista_repetidos
                     lista_repetidos[indice_lista_repetidos] = termo
                     indice_lista_repetidos +=1
+                elif termo_comparacao == lista[-1] and (contador_comparacao == (tamanho_total-1)):
+                    inicio_lista_a_comparar +=1
+                    contador_comparacao = 0
+                    break
+                else:
+                    continue
+                    # essa variavel precisa ser acrescida toda vez que vamos passar por um termo 
             
     return lista_repetidos
 
-print(retirar_repetidos([5, 2, 9, 1, 5, 6, 2]))
+tamanho = tamanho_lista([5, 2, 9, 1, 5, 6, 2])
+print(retirar_repetidos([5, 2, 9, 1, 5, 6, 2], tamanho))
