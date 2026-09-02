078c4265bd17b40a66eaf6a6e317d42051874dc2
nathalidev <nathalidorio@gmail.com>
2026-09-02 16:47:50 -0300

    finalização do exercicio com uma função para informar os repetidos outro para gerar a lista limpa, outro para informar o tamanho da lista limpa e o ordenador crescente

--- aulas_do_curso/lista_unica_ordenada.py
+++ aulas_do_curso/lista_unica_ordenada.py
@@ -48,18 +48,26 @@ def lista_sem_repetidos (lista_original, repetidos):
     return lista_sem_repetidos
     
 lista_limpa = lista_sem_repetidos(lista, termos_repetidos)
-
 # agora é só ordenar de forma crescente
 
-def ordenador_crescente (lista_limpa):
-    indice_lista_limpa = 1
-    for termo in lista_limpa:
-        for termo2 in lista_limpa[contador::]:
-            if termo > termo2:
-                lista_limpa[indice_lista_limpa] = termo
-                lista_limpa[indice_lista_limpa-1] = termo2
-            indice_lista_limpa += 1
+def tamanho (lista_sem_repetidos):
+    contador = 0
+    for i in lista_sem_repetidos:
+        contador +=1
+    return contador
+
+tamanho_lista = tamanho(lista_limpa)
+print(tamanho_lista)
+
+def ordenador_crescente (lista_limpa, tamanho_lista):
+    contador = 0
+    while contador < tamanho_lista:
+        indice = contador + 1  
+        while indice < tamanho_lista:
+            if lista_limpa [contador] > lista_limpa[indice]:
+                lista_limpa[contador], lista_limpa[indice] = lista_limpa[indice], lista_limpa[contador]
+            indice += 1
         contador += 1
     return lista_limpa
 
-print(ordenador_crescente(lista_limpa))
+print(ordenador_crescente(lista_limpa, tamanho_lista))
